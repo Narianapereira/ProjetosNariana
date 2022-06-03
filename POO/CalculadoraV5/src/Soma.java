1eb45012b6e0c85ddbd19cfb30ba6a58b42fd6b5
Narianapereira <nariana.pereira@outlook.com>
2022-06-03 13:57:37 -0300

    Commit atualizacao projetos

---
+++ POO/CalculadoraV5/src/Soma.java
@@ -0,0 +1,27 @@
+
+public class Soma implements IOperacao{
+
+	private double num1;
+	private double num2;
+// o metodo get nao está sendo usado? Pois somente estou setando o valor da variável num1 do principal aqui no atributo num1
+	public double getNum1() {
+		return this.num1;
+	}
+
+	public void setNum1(double num1) {
+		this.num1 = num1;
+	}
+
+	public double getNum2() {
+		return this.num2;
+	}
+
+	public void setNum2(double num2) {
+		this.num2 = num2;
+	}
+
+	public double calcula() {
+		double soma = num1 + num2;
+		return soma;
+	}
+}
---
+++ POO/CalculadoraV7/src/modelo/Divisao.java
@@ -0,0 +1,9 @@
+package modelo;
+
+public class Divisao extends Operacoes {
+
+	public double calcula() {
+		double divi = num1 / num2;
+		return divi;
+	}
+}
